313bddaea178d697eb59a5d0f73abccd4124654f
yaningwang80381 <56126339+yaningwang80381@users.noreply.github.com>
2020-08-05 14:04:59 -0400

    Add files via upload

---
+++ generatedata.m
@@ -0,0 +1,15 @@
+%% An example using one phantom. If testing multiple phantoms,please repeat the procedure
+clear all
+close all
+clc
+load('channel_data');%load corresponding channeldata
+load('multi.mat');%load corresponding groundtruth 
+ for i = 1:75 %75 is the number of single plane wave
+ temp1 = channel_data.data(:,:,i);
+ temp2 = temp1(1:1527,1:size(temp1,2));
+ temp4 = imresize(temp2,[256 256],'bilinear');
+ temp4 = rescale(temp4,-60,0);
+ temp5 = [multi,temp4];temp5 = rescale(temp5,0,1);
+ %imwrite(temp5, strcat(num2str(i),'.jpg'));
+ end
+%the .jpg file will be used in next step 'saved_model'.ipynb
---
+++ npytomat.m
@@ -0,0 +1,18 @@
+%% please use the loop if there are more than one test data
+for i = 1:9
+filename = strcat('C:\Users\Downloads\',num2str(i),'.npy');
+
+[arrayShape, dataType, fortranOrder, littleEndian, totalHeaderLength, npyVersion] = readNPYheader(filename);
+
+if fortranOrder
+    f = memmapfile(filename, 'Format', {dataType, arrayShape, 'd'}, 'Offset', totalHeaderLength);
+    image(f.Data.d)
+else 
+    f = memmapfile(filename, 'Format', {dataType, arrayShape(end:-1:1), 'd'}, 'Offset', totalHeaderLength);
+    
+    tmp = f.Data.d;
+    img = permute(tmp, length(arrayShape):-1:1); 
+    img = reshape(img,[256,256]);
+    imshow(img,[])%
+end
+end
